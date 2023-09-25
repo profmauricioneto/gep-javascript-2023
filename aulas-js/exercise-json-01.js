f1a638ad0531f268114ccde8caaf18d373e74908
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2023-09-25 18:03:06 -0300

    add: exercise with javascript objects.

---
+++ aulas-js/exercise-json-01.js
@@ -0,0 +1,7 @@
+const person = {
+    name: 'maurício moreira neto',
+    age: 33,
+    occupation: 'professor'
+};
+
+console.log(`Hello, my name is ${person.name} and i has ${person.age} years old and my occupation is ${person.occupation}.`);
---
+++ aulas-js/exercise-json-02.js
@@ -0,0 +1,41 @@
+"use strict";
+const prompt = require("prompt-sync")();
+let stock = [];
+
+// Main function
+function main() {
+  let option;
+  let product;
+  do {
+    product = {};
+    product.name = prompt("Product name: ");
+    product.price = prompt("Product price: ");
+    product.quantity = prompt("Product quantity: ");
+
+    stock.push(product);
+    option = prompt("Do you want to add a product? (y/n) ");
+  } while (option == "y");
+
+  console.log(stock);
+  let productName = prompt("What product do you want to check? ");
+  checkInventory(productName);
+}
+
+// Check if the product is available
+let checkInventory = (productName) => {
+    stock.forEach(element => {
+        if (element.name === productName) {
+            if (element.quantity > 0) {
+                console.log(`The product ${element.name} is available.`);
+                console.log(`It has ${element.quantity} units in stock.`);
+            } else {
+                console.log(`The product ${element.name} is not available.`);
+            }
+        } else {
+            console.log(`The product ${element.name} is not available.`);
+        }
+    });
+};
+
+// Main loop
+main();
